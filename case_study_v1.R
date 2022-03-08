af724ba6ba906c6bba4934290087d6f1e5f5df75
Francisco.Colina <franco.collina@gmail.com>
2022-03-08 08:28:32 -0600

    Case Study - Section E

--- case_study_v1.R
+++ case_study_v1.R
@@ -15,7 +15,7 @@ all_var <- c(2,6,48,8,9,65)
 
 #Remove all NA's only
 df.altmann <- df %>%
-              select(var) %>%
+              select(all_var) %>%
               na.omit %>%
               rename(c(working_capital = Attr2,retained_earnings=Attr6,
                        EBITDA=Attr48,book_value=Attr8,sales=Attr9))
@@ -35,6 +35,15 @@ summary(log.model)
 ## Sales and EBITDA are not significative, the coeficients are to high
 
 #Compute the confusion matrix and the overall fraction of correct predictions
+log.probs = predict(log.model,type="response")
+
+log.pred.glm <- as.factor(ifelse(log.probs >= 0.5, 
+                                            1, 0))
+
+confusionMatrix(log.pred.glm, as.factor(df.altmann$class))
+
+## The fraction that is miss classified is 
+163/(5208+161+2)
 
 
 # E using the validation set approach
@@ -52,8 +61,12 @@ predict.log <- predict(log.model.train,newdata=test,type="response")
 #Probability of 0.5
 predicted_class <- as.factor(ifelse(predict.log >= 0.5, 
                                       1, 0))
+
+
 #Mean Square Prediction Error
 
 confusionMatrix(predicted_class, as.factor(test$class))
 
 #F.- Using KNN with 1
+
+
